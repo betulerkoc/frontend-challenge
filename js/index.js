47f5b54fc428220560370b149e5bfceef2b4b63f
Betul Erkoc <betulfatmaerkoc@gmail.com>
2021-08-08 13:53:41 +0300

    add eventlistener

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ class APIService {
     const url = APIService._constructUrl("responses");
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data)
+    console.log(data);
     return data[0][0].params;
   }
   static async fetchCampaigns() {
@@ -22,9 +22,10 @@ class APIService {
 
 class App {
   static async run() {
+    const defaultIndex = 0;
     const response = await APIService.fetchResponses();
-    Home.renderSideBarTitles(response.userCategories);
-    Home.renderProductCards(response.recommendedProducts);
+    Home.renderSideBarTitles(response);
+    Home.renderProductCards(response, defaultIndex);
   }
 }
 
@@ -32,25 +33,29 @@ class Home {
   static sidebar = document.getElementById("sidebar");
   static productCards = document.getElementById("productCards");
 
-  static renderSideBarTitles(userCategories) {
-    const sideBarItem = document.createElement("container");
-    userCategories.map((item) => {
+  static renderSideBarTitles(response) {
+    const sideBarItem = document.createElement("ul");
+    response.userCategories.map((item, index) => {
+      console.log(item);
       const content = `
           <div class="nav-item">
-              <p>${item}</p>
+              <li>                       
+                <p class="nav-link" category-link-id="${index}" href="#" role="button"> ${item}</p>
+              </li>
           </div>
               `;
       sideBarItem.innerHTML += content;
     });
     this.sidebar.appendChild(sideBarItem);
+    Events.bindCategoryListener(response);
   }
 
-  static renderProductCards(recommendedProducts) {
+  static renderProductCards(response, index) {
     const productCard = document.createElement("div");
     productCard.className = "row";
-
-    const porductCardData = Object.entries(recommendedProducts)[0][1];
-
+    const porductCardData = Object.entries(response.recommendedProducts)[
+      index
+    ][1];
     const card = (item) => `
           <li class=card>
             <img src="${item.image}" class="card-img-top">
@@ -59,7 +64,7 @@ class Home {
           </li>
       `;
 
-    let content = `
+    const content = `
           <div class="col-12">
               <div class="product-list">
                   <ol class="productCard scroller">
@@ -74,6 +79,18 @@ class Home {
 
     productCard.innerHTML = content;
     this.productCards.appendChild(productCard);
+  }
+}
+
+class Events {
+  static bindCategoryListener(response) {
+    console.log("hello");
+    const categoryLinks = document.querySelectorAll("[category-link-id]");
+    categoryLinks.forEach((link, index) => {
+      link.addEventListener("click", function (e) {
+        Home.renderProductCards(response, index);
+      });
+    });
   }
 }
 
